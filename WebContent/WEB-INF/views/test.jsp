54dd2b899d224ed6b68985cdeded77b451514079
lunea <lunea@KOUSETSU>
2021-01-01 11:02:20 +0900

    mvc2에서 사이트메시 사용 테스트

---
+++ WebContent/WEB-INF/views/test.jsp
@@ -0,0 +1,3 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h2>사이트메시 설정 테스트</h2>
--- src/test/TestClass.java
+++ src/test/TestClass.java
@@ -1,10 +1,12 @@
 package test;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class TestClass {
-	public static void main(String[] args) {
-		System.out.println("안녕하세요 도심창고입니다.");
-		// 깃허브 탁강현
-		System.out.println("합리적인 가격에 생활의 질을 높여보세요");
-		//  깃허브 손석현 
+import service.CommandProcess;
+
+public class TestClass implements CommandProcess{	
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
+		return "test";
 	}
 }
