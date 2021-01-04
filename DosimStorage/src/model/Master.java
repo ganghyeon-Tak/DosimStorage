d3b8063d50fecffb22d6393683abdef5cbc45144
dahyelee0130 <76485815+dahyelee0130@users.noreply.github.com>
2021-01-04 14:58:25 +0900

    dto 작성

---
+++ DosimStorage/src/model/Master.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Master {
+	public String master_id;
+	public String master_pw;
+
+	public String getMaster_id() {
+		return master_id;
+	}
+
+	public void setMaster_id(String master_id) {
+		this.master_id = master_id;
+	}
+
+	public String getMaster_pw() {
+		return master_pw;
+	}
+
+	public void setMaster_pw(String master_pw) {
+		this.master_pw = master_pw;
+	}
+
+}
