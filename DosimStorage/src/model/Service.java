019c4a9e167dcb57337811d1402925074e20feb1
KOUSETSU\lunea <ichigo_pasta0731@yahoo.co.jp>
2021-01-04 14:31:50 +0900

    dto작성

---
+++ DosimStorage/src/model/Service.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Service {
+	private String s_kind;
+	private String s_price;
+	public String getS_kind() {
+		return s_kind;
+	}
+	public void setS_kind(String s_kind) {
+		this.s_kind = s_kind;
+	}
+	public String getS_price() {
+		return s_price;
+	}
+	public void setS_price(String s_price) {
+		this.s_price = s_price;
+	}	
+}
