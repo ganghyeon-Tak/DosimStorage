3163948ac476a84efcea2a5753a1642d8b062413
KOUSETSU\lunea <ichigo_pasta0731@yahoo.co.jp>
2021-01-04 14:44:33 +0900

    자료형 수정

--- DosimStorage/src/model/Service.java
+++ DosimStorage/src/model/Service.java
@@ -2,17 +2,18 @@ package model;
 
 public class Service {
 	private String s_kind;
-	private String s_price;
+	private int s_price;
 	public String getS_kind() {
 		return s_kind;
 	}
 	public void setS_kind(String s_kind) {
 		this.s_kind = s_kind;
 	}
-	public String getS_price() {
+	public int getS_price() {
 		return s_price;
 	}
-	public void setS_price(String s_price) {
+	public void setS_price(int s_price) {
 		this.s_price = s_price;
-	}	
+	}
+	
 }
