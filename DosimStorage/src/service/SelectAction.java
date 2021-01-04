ac92447fb28072d2509e2e8d67747c0b1bb17f0b
dahyelee0130 <76485815+dahyelee0130@users.noreply.github.com>
2021-01-04 21:21:26 +0900

    mypage관련파일추가(이용중인서비스제외)

---
+++ DosimStorage/src/service/SelectAction.java
@@ -0,0 +1,21 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.MemberDao;
+import model.Member;
+
+public class SelectAction implements CommandProcess {
+
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		String m_id = (String)session.getAttribute("m_id");
+		MemberDao md = MemberDao.getInstance();
+		Member member = md.select(m_id);
+		request.setAttribute("member", member);
+		return "select";
+	}
+
+}
