ee6071c140e44fc4d7a8a3756490a2aa0a12f61d
dahyelee0130 <76485815+dahyelee0130@users.noreply.github.com>
2021-01-04 14:50:21 +0900

    dto작성

---
+++ DosimStorage/src/model/Account.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Account {
+
+	private String account_no;
+	private String bank;
+	private String account_name;
+
+	public String getAccount_no() {
+		return account_no;
+	}
+
+	public void setAccount_no(String account_no) {
+		this.account_no = account_no;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public String getAccount_name() {
+		return account_name;
+	}
+
+	public void setAccount_name(String account_name) {
+		this.account_name = account_name;
+	}
+
+}
