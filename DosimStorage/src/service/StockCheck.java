e31a623c9d281338fca7c2f060e8880fb5f6cb7c
KOUSETSU\lunea <ichigo_pasta0731@yahoo.co.jp>
2021-01-06 19:06:36 +0900

    재고체크로직 미완성

--- DosimStorage/src/service/StockCheck.java
+++ DosimStorage/src/service/StockCheck.java
@@ -9,17 +9,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.Stock_viewDao;
 
+
 //	재고체크해서 매진된 항목 사용불가 표시하는 것이 목적
 public class StockCheck implements CommandProcess {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
 		Stock_viewDao svd = Stock_viewDao.getInstance();
+			
 		List<String> list = new ArrayList<String>();
 		// 해당 타입의 창고가 모두 소진된 지점이 타입+지점코드 형태로 list에 저장됨
-		list = svd.select();
-		System.out.println(list.get(0));
-		return "stockCheck";
+		list = svd.select();		
+		int soldout_len = list.size();
+		System.out.println(soldout_len);
+		System.out.println(list);
+		request.setAttribute("soldout", list);
+		request.setAttribute("soldout_len", soldout_len);
+		return "orderForm";
 	}
 
 }
