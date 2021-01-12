98b667d92f72dbc1f3afcdfe184d5de474eecadf
Son <Son@DESKTOP-VGFB6H3>
2021-01-12 17:13:20 +0900

    네비게이션에서 지점선택 연결 완성. 네비 z-index 수정

--- DosimStorage/src/service/BranchInfo.java
+++ DosimStorage/src/service/BranchInfo.java
@@ -7,7 +7,9 @@ public class BranchInfo implements CommandProcess {
 
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+		String br = request.getParameter("br");
+		if (br.equals("") || br.equals(null)) br = "1";
+		request.setAttribute("br", br);
 		return "branchInfo";
 	}
 
